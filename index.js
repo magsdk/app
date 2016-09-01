d65ad150fe92a587101f96b376695197030c9190
Yaroslav <y.surilov@infomir.com>
2016-09-01 18:59:39 +0300

    add app.exit() and force reload app after location changes

--- index.js
+++ index.js
@@ -19,20 +19,118 @@ app.ready = function () {
     // if ( this.events['show'] ) {
     //     this.emit('show');
     // }
-
     window.core.call('app:ready');
+};
+
+
+/**
+ * Exit from app.
+ * Destroy all application instance.
+ * If callback function provided, and callback returns boolean 'true', application will stay alive.
+ *
+ * @fires module:/stb/app#exit
+ *
+ * @param [callback] provide callback if u want to handle exit result, or cancel it
+ */
+app.exit = function ( callback ) {
+    var ModalMessage = require('magsdk/component-modal'),
+        LayoutList   = require('magsdk/component-layout-list'),
+        rc           = require('stbsdk/rc'),
+        previousFocus = app.activePage.activeComponent,
+        exitModal;
+
+    app.activePage.add(exitModal = new ModalMessage({
+        title: _('Exit'),
+        events:{
+            show: function () {
+                this.children[0].focus();
+            },
+            hide: function () {
+                previousFocus.focus();
+            }
+        },
+        children:[
+            new LayoutList({
+                size:2,
+                focusIndex:0,
+                data:[
+                    {
+                        items: [
+                            {
+                                value: _('Exit')
+                            }
+                        ],
+                        click: function () {
+                            if ( typeof callback === 'function' ) {
+                                if ( callback(true) ) {
+                                    exitModal.hide();
+                                    exitModal.remove();
+                                    return;
+                                }
+                            }
+                            if ( app.events['exit'] ) {
+                                app.emit('exit');
+                            }
+
+                            exitModal.hide();
+                            exitModal.remove();
+                            core.call('exit');
+                        }
+                    },
+                    {
+                        items: [
+                            {
+                                value: _('Cancel')
+                            }
+                        ],
+                        click: function () {
+                            if ( typeof callback === 'function' ) {
+                                callback(false);
+                            }
+                            exitModal.hide();
+                            exitModal.remove();
+                        }
+                    }
+                ],
+                events: {
+                    keydown: function ( event ) {
+                        LayoutList.prototype.defaultEvents.keydown.call(this, event);
+                        if ( event.keyCode === rc.codes.back ) {
+                            event.stop = true;
+                            if ( typeof callback === 'function' ) {
+                                callback(false);
+                            }
+                            exitModal.hide();
+                            exitModal.remove();
+                        }
+                    }
+                }
+            })
+        ]
+    }));
+
+    exitModal.show();
+    exitModal.focus();
 };
 
 
 events.load = function ( event ) {
     window.core = window.parent.getCoreInstanse(window, app);
 
-    core.once('load', function () {
-        if ( app.events[event.type] ) {
+    if ( core.ready ) {
+        if ( app.events['load'] ) {
             // notify listeners
-            app.emit(event.type, event);
+            app.emit('load', {});
         }
-    });
+    } else {
+        core.once('load', function () {
+            core.ready = true;
+            if ( app.events[event.type] ) {
+                // notify listeners
+                app.emit(event.type, event);
+            }
+        });
+    }
 };
 
 
