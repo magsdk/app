c11cfc24ccaa2ee25e56c5b3f91f3da5eecf1667
boris <aleynikov.boris@gmail.com>
2016-10-18 17:36:59 +0300

    fix path

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * @license The MIT License (MIT)
+ * @copyright Stanislav Kalashnik <darkpark.main@gmail.com>
+ */
+
+'use strict';
+
+var app = require('./lib/main');
+
+
+module.exports = app;
